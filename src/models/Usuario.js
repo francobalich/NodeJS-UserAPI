cc2a16fac30c44a3b4bfc5b7cdfa252802f10348
Franco Balich <franco.balich@gmail.com>
2023-07-10 19:14:11 -0300

    Add user model

---
+++ src/models/Usuario.js
@@ -0,0 +1,23 @@
+import { Schema, model } from "mongoose";
+
+const UsuarioSchema = Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    surname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+})
+
+export const Usuario = model('Usuario', UsuarioSchema)
