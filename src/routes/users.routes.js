d917bb351f39c695f223b7baceb3e5db678a6a86
Franco Balich <franco.balich@gmail.com>
2023-07-10 23:00:45 -0300

    Feat: Add validations

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,33 @@
+import { check } from 'express-validator'
+import { getLogin, postRegisterUser, putModifiedUser, deleteUser } from '../controllers/users.controllers.js'
 import { Router } from 'express'
-import { getLogin, postRegisterUser, putModifiedUser, deleteUser} from '../controllers/users.controllers.js'
+import { validarCampos } from '../middleware/validarCampos.js'
 
 export const userRouter = Router()
 
-userRouter.get('/', getLogin)
+userRouter.get('/',
+  [
+    check('email', 'El email es obligatorio.').isEmail(),
+    check('password', 'La contraseña debe tener como minimo 6 caracteres.').isLength({ "min": 6 }),
+    validarCampos
+  ], getLogin)
 
-userRouter.post('/', postRegisterUser)
+userRouter.post('/',
+  [
+    check('name', 'El nombre es obligatorio.').not().isEmpty(),
+    check('surname', 'El apellido es obligatorio.').not().isEmpty(),
+    check('email', 'El email es obligatorio.').isEmail(),
+    check('password', 'La contraseña debe tener como minimo 6 caracteres.').isLength({ "min": 6 }),
+    validarCampos
+  ], postRegisterUser)
 
-userRouter.put('/:id', putModifiedUser)
+userRouter.put('/:id',
+  [
+    check('name', 'El nombre es obligatorio.').not().isEmpty(),
+    check('surname', 'El apellido es obligatorio.').not().isEmpty(),
+    check('email', 'El email es obligatorio.').isEmail(),
+    check('password', 'La contraseña debe tener como minimo 6 caracteres.').isLength({ "min": 6 }),
+    validarCampos
+  ], putModifiedUser)
 
 userRouter.delete('/:id', deleteUser)
